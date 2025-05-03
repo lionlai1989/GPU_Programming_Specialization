2f91a6840a8a9b0ed79072be6875bf8bb5317a1d
lai <lionlai1989@gmail.com>
2025-05-03 15:05:19 +0200

    finish 01-Threads_Blocks_Grids/thread_linear_addressing.cu

--- 01-Threads_Blocks_Grids/thread_linear_addressing.cu
+++ 01-Threads_Blocks_Grids/thread_linear_addressing.cu
@@ -8,7 +8,7 @@
 #include <cuda_runtime.h>
 #include <thrust/device_vector.h>
 
-__global__ void vecAdd_thread_linear_addressing(int *a, size_t num_elements) {
+__global__ void vecAdd(int *a, size_t num_elements) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     printf("vecAdd start tid: %d\n", tid);
     while (tid < num_elements) {
@@ -27,8 +27,8 @@ __global__ void vecSub(int *a, size_t num_elements) {
     }
 }
 
-// nvcc thread_linear_addressing.cu && ./a.out 1024 32 16
-// nvcc thread_linear_addressing.cu && ./a.out 1030 32 16
+// nvcc thread_linear_addressing.cu && ./a.out 1024 32 16 > out_1024_32_16.txt
+// nvcc thread_linear_addressing.cu && ./a.out 1030 32 16 > out_1030_32_16.txt
 int main(int argc, char *argv[]) {
     int num_elements = std::atoi(argv[1]);
     int threads_per_block = std::atoi(argv[2]);
@@ -44,16 +44,16 @@ int main(int argc, char *argv[]) {
     thrust::device_vector<int> d_sums(num_elements);
     int *d_sums_ptr = thrust::raw_pointer_cast(&d_sums[0]);
 
-    std::cout << "Launch " << blocks_per_grid * threads_per_block << " threads. " << "(" << blocks_per_grid << "x"
-              << threads_per_block << ")," << "(blocks_per_grid, threads_per_block)" << std::endl;
+    std::cout << "Launch " << blocks_per_grid * threads_per_block << " vecAdd threads. " << "(" << blocks_per_grid
+              << "x" << threads_per_block << ")," << "(blocks_per_grid, threads_per_block)" << std::endl;
     // Host is not blocked, so it will not wait for the kernel to finish
-    vecAdd_thread_linear_addressing<<<blocks_per_grid, threads_per_block>>>(d_sums_ptr, num_elements);
+    vecAdd<<<blocks_per_grid, threads_per_block>>>(d_sums_ptr, num_elements);
 
     // Automatically calculate the number of blocks per grid
     blocks_per_grid = (num_elements + threads_per_block - 1) / threads_per_block;
 
-    std::cout << "Launch " << blocks_per_grid * threads_per_block << " threads. " << "(" << blocks_per_grid << "x"
-              << threads_per_block << ")," << "(blocks_per_grid, threads_per_block)" << std::endl;
+    std::cout << "Launch " << blocks_per_grid * threads_per_block << " vecSub threads. " << "(" << blocks_per_grid
+              << "x" << threads_per_block << ")," << "(blocks_per_grid, threads_per_block)" << std::endl;
 
     vecSub<<<blocks_per_grid, threads_per_block>>>(d_sums_ptr, num_elements);
 
